0538996b17b2ac1f367dcfb1fe4f97a76776c9fe
Ondra Stanek <ondrastan42@gmail.com>
2026-05-06 10:40:53 +0200

    env fix

--- vibecoding-app/lib/supabase.ts
+++ vibecoding-app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://jrocmktjzobcrxbgcwke.supabase.co/rest/v1/",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impyb2Nta3Rqem9iY3J4Ymdjd2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTA4ODgsImV4cCI6MjA5MzM4Njg4OH0.kIcgCxBaRemw6v1mo1FSmEqSm6RXrKFZG7yn0-wDhgg"
-);
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);;
